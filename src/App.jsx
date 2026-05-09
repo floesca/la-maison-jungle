bab19345298ad5f35a82235d78db9b38e341b3b0
Florence Escanye <f.escanye@gmail.com>
2026-05-09 21:36:56 +0200

    ajout shopping list

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Banner from './components/banner'
 import Cart from './components/Cart'
+import ShoppingList from './components/ShoppingList'
 
 const App = () => {
   return (
     <div>
       <Banner />
       <Cart />
+      <ShoppingList />
     </div>
   )
 }
